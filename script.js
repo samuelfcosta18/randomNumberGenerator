51336e392106e049ac72eeac496c524c84bf850a
Samuel Freitas <samuelfreitas7895@gmail.com>
2024-09-02 22:37:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function randomNumber() {
     let input1 = document.getElementById("valor1").value
+    if (input1 == "") {
+        input1 == 0
+    }
     let input2 = document.querySelector("#valor2").value
     let resultado = document.querySelector("#generatedNumber")
-    let language = document.querySelector("#language")
 
     const min = Math.floor(input1)
     const max = Math.floor(input2)
     const result = Math.floor(Math.random() * (max - min)) + min
     console.log(result)
     resultado.value = `${result}`
-
-
 }
 function changeLanguage() {
     let language = document.querySelector("#language")
@@ -26,5 +26,14 @@ function changeLanguage() {
         document.querySelector("#valor2").placeholder = "Insert here the maximum value"
         document.querySelector("#number").innerHTML = "The generated number is:"
         document.querySelector("#botao1").innerHTML = "Generate"
+    }
+}
+function darkMode() {
+    if (document.querySelector("main").classList.contains("light")) {
+        document.querySelector("main").classList.add("dark")
+        document.querySelector("main").classList.remove("light")
+    } else {
+        document.querySelector("main").classList.remove("dark")
+        document.querySelector("main").classList.add("light")
     }
 }
